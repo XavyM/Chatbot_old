968b86e7fdb4b754a2a014673688326b562b4f62
xavier musk <xavier@xaviers-macbook-air.local>
2017-01-27 09:50:58 -0800

    Initial Commit

---
+++ Chatbot app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chatbot app
+//
+//  Created by xavier musk on 1/27/17.
+//  Copyright © 2017 Photo-ish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
